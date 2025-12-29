66abf3b7b16ec01c16ee95de6153d58201e77393
Shashank Mondrati <dandiya14@gmail.com>
2025-12-29 18:37:07 -0500

    changes again

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 //imported the component
 import Counter from "./components/Counter";
+import Color from "./components/Color";
+import { useEffect } from "react";
 function App() {
+  function SetTitle() {
+    useEffect(() => {
+      document.title = "React Application";
+    }, []);
+  }
   return (
     <>
       {/* Calling the component using the import statement and calling it here */}
+      <SetTitle />
       <Counter />
+      <Color />
     </>
   );
 }
