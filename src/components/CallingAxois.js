a33a94052230e0e5106ae37a7f4ec8e50b81ae1d
Shashank Mondrati <dandiya14@gmail.com>
2026-01-01 12:43:02 -0500

    axios api calling

---
+++ src/components/CallingAxois.js
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+export default function CallingAxois() {
+  //using axios to make API calling third way of API calling
+  const [data, setData] = useState(null);
+  const url = ``;
+  useEffect({});
+  return <></>;
+}
