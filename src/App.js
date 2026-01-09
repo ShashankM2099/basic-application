f9f8e8693df009c604531e5b946ea0c0d80af558
Shashank Mondrati <dandiya14@gmail.com>
2026-01-09 13:25:03 -0500

    fixed changed

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
       {/* importing the header */}
       <Header />
       <>
+        hello
         {/* importing Routing Codes */}
         <RouterCode />
       </>
--- src/components/RouterCode.js
+++ src/components/RouterCode.js
@@ -4,13 +4,13 @@ import Counter from "./Counter";
 import Form from "./Form";
 import Color from "./Color";
 import CallingAPI from "./CallingAPI";
-import CallingAxois from "./CallingAxois";
+import CallingAxios from "./CallingAxios";
 import CardStyle from "./CardStyle";
 
 export default function RouterCode() {
   return (
     <>
-      <nav className="routes-contanier" style={{ padding: "10px" }}>
+      <nav className="routes-container" style={{ padding: "10px" }}>
         <Link to="/">Counter</Link> | <Link to="/form">Form</Link> |{" "}
         <Link to="/color">Color</Link> | <Link to="/api">Fetch API</Link> |{" "}
         <Link to="/axios">Axios</Link> | <Link to="/card">About US</Link>
@@ -21,7 +21,7 @@ export default function RouterCode() {
         <Route path="/form" element={<Form />} />
         <Route path="/color" element={<Color />} />
         <Route path="/api" element={<CallingAPI />} />
-        <Route path="/axios" element={<CallingAxois />} />
+        <Route path="/axios" element={<CallingAxios />} />
         <Route path="/card" element={<CardStyle />} />
       </Routes>
     </>
